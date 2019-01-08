e3dc5709e91209676533579f97aa973cc85312a4
Emile Kratiroff <emile@kratiroff.com>
2019-01-08 10:22:43 +0100

    Cross over points questions

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ You should be used to this, it's a typical coding question.
 
 The program tells the story of a user visiting a library. They search for a book, rent it, return it and then donate a book.
 
-The code is badly written in terms of speed, resilience and style. Refactor it so it looks like something you'd like to read.
+The code is badly written in terms of resilience, speed and style. Refactor it!
 
 ## 3. Vue.js
 
@@ -17,7 +17,7 @@ Watch this: https://www.youtube.com/watch?v=aqAUmgE3WyM
 
 Build a virtual useless box using Vue.js!
 
-It must not take you more than 45min.
-It doesn't have to be deployed anywhere, running it on localhost would be enough.
-Unlike real-life projects, this will not evolve into something greater, use the simplest tools for the job.
-Feel free to add dependencies if it makes your life easier.
+* It must not take you more than 30min
+* It doesn't have to be deployed anywhere, running it on localhost would be enough
+* Unlike real-life projects, this will not evolve into something greater, use the simplest tools for the job
+* Feel free to add dependencies if it makes your life easier
--- programming.js
+++ programming.js
@@ -1,3 +1,50 @@
-/**
-TODO: open the interview book and pick one!
- */
+'use strict';
+
+const assert = require('assert');
+
+/*
+Identify cross-over indices for two equal-length lists of prices.
+
+Parameters:
+	pricesOne, pricesTwo: lists of prices (ordered by time)
+
+Returns:
+	list of crossover points
+
+Each item in the returned list is a list [timeIndex, higherIndex], where:
+	- timeIndex is the crossover time index (when it happens)
+	- higherIndex indicates which price becomes higher at timeIndex: either 1 for first list or 2 for second list
+
+There are no crossovers before both price lists have values that are not null.
+You can start making comparisons from the point at which both have number values.
+*/
+
+function crossOvers(pricesOne, pricesTwo) {
+	const results = [];
+
+	let startIndex = 0;
+	while (typeof pricesOne[startIndex] !== 'number' || typeof pricesTwo[startIndex] !== 'number') {
+		startIndex += 1;
+	}
+
+	for (let i in Array(pricesOne.length - startIndex + 1).keys()) {
+		if (pricesOne[i - 1] > pricesTwo[i - 1] && pricesOne[i] < pricesTwo[i]) {
+			results.push([i, 2]);
+		} else if (pricesOne[i - 1] < pricesTwo[i - 1] && pricesOne[i] > pricesTwo[i]) {
+			results.append([i, 1]);
+		}
+	}
+
+	return results;
+}
+
+
+// test case A
+const p1a = [1, 2, 4, 5];
+const p2a = [0, 2.5, 5, 3];
+assert.deepEqual(crossOvers(p1a, p2a), [[1, 2], [3, 1]]);
+
+// test case B
+const p1b = [null, 2.5, 3.5, 4.5, 4.5, 3.5, 2.5, 1.5, 3.5, 3.5];
+const p2b = [null, null, 3.0, 4.0, 4.333333333333333, 4.0, 3.0, 2.0, 3.0, 2.6666666666666665];
+assert.deepEqual(crossOvers(p1b, p2b), [[5, 2], [8, 1]]);
